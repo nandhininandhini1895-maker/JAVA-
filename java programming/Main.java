2727f9c001c5b6dfa2081724ce6732bb42a16974
nandhininandhini1895-maker <nandhininandhini1895@gmail.com>
2026-07-24 10:36:04 +0530

    Add files via upload

---
+++ java programming/Main.java
@@ -0,0 +1,20 @@
+class Main{
+public static void main(String args[]){
+int x = 5;
+float y = 1.5;
+char z = "K";
+long a = 2000;
+byte b = 90;
+double c = 8.543;
+sort d = 350;
+boolean e = True;
+System.out.println(x);
+System.out.println(y);
+System.out.println(z);
+System.out.println(a);
+System.out.println(b);
+System.out.println(c);
+System.out.println(d);
+System.out.println(e);
+}
+}
