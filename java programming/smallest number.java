79387cb74f268355fae4a3e53eb4832cc93f9d5b
nandhininandhini1895-maker <nandhininandhini1895@gmail.com>
2026-07-24 10:34:22 +0530

    Add files via upload

---
+++ java programming/smallest number.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class SmallestDigit {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+        int smallest = 9;
+
+        while (num != 0) {
+            int digit = num % 10;
+
+            if (digit < smallest) {
+                smallest = digit;
+            }
+
+            num /= 10;
+        }
+
+        System.out.println(smallest);
+    }
+}
